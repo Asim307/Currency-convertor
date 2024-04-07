5362dcf00e55cf63f0c2c1f8bd60657a9ad8b7f4
Asim307 <asimshaikhby0097@gmail.com>
2024-04-07 13:02:36 -0700

    cli based currency convertor

---
+++ index.js
@@ -0,0 +1,35 @@
+
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    DHR: 3.67,
+    PKR: 280
+};
+let answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "DHR", "PKR"],
+    },
+    {
+        name: "to",
+        message: "enter to currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "DHR", "PKR"],
+    },
+    {
+        name: "amount",
+        message: "enter amount",
+        type: "number"
+    },
+]);
+let fromAmount = currency[answer.from];
+let toAmount = currency[answer.to];
+let Amount = answer.amount;
+let basedAmount = Amount / fromAmount;
+let convertedAmount = basedAmount * toAmount;
+console.log(convertedAmount);
